bc57a066c9c10d094ac8ca89845f3cd215861ea2
Enner Pérez <ennerperez@gmail.com>
2019-01-03 17:38:56 -0500

    NuGet Id Update

--- src/Identity.EntityFramework/Properties/AssemblyInfo.cs
+++ src/Identity.EntityFramework/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Identity.EntityFramework")]
-[assembly: AssemblyProduct("Identity.EntityFramework")]
+[assembly: AssemblyTitle("IdentityFramework.EntityFramework")]
+[assembly: AssemblyProduct("IdentityFramework.EntityFramework")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("17d56636-0d8d-401e-9484-da7965dcf45a")]
